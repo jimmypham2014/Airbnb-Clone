db703c38780749b5b8c9b87cbbabbb1ab8b4fecc
jimmy <jimmyfam96@gmail.com>
2022-10-27 20:21:36 -0700

    awsS3

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,3 +1,6 @@
+import { singlePublicFileUpload, singleMulterUpload } from '../../awsS3';
+
+
 const express = require('express');
 const router = express.Router();
 const {restoreUser,requireAuth} = require('../../utils/auth')
@@ -490,7 +493,29 @@ router.post('/:id/bookings', requireAuth,validateBooking,async (req,res) =>{
 
     // AVERAGE number of review
 
-    
+
+    router.post(
+        "/",
+        singleMulterUpload("image"),
+        validateSignup,
+        asyncHandler(async (req, res) => {
+          const { email, password, username } = req.body;
+          const profileImageUrl = await singlePublicFileUpload(req.file);
+          const user = await User.signup({
+            username,
+            email,
+            password,
+            profileImageUrl,
+          });
+      
+          setTokenCookie(res, user);
+      
+          return res.json({
+            user,
+          });
+        })
+      );
+
 
 
 module.exports = router;
