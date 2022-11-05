5eae31d75c0d1c3ca983388e0c6298d8d837c334
jimmy <jimmyfam96@gmail.com>
2022-11-05 11:20:34 -0700

    asdf

--- backend/db/seeders/20220914064950-demo-spot.js
+++ backend/db/seeders/20220914064950-demo-spot.js
@@ -20,8 +20,7 @@ module.exports = {
       lat: 37.7645358,
       lng: -122.4730327,
       name: "Beverly Hills Maison",
-      previewImage: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-47354666/original/b92fc905-70ea-449a-aa95-c79ade3ceadb.jpeg?im_w=1200',
-      description: "Private, gated French country chateau-inspired home sits at the end of a cul de sac on nearly 2 acres of land and is surrounded by stately trees, mature landscaping, and verdant canyon views. Beautifully living room with fireplace, formal dining room, large kitchen, and light-filled breakfast area. Spectacular canyon views from every room and nearby access to Beverly Hills, and the valley. Expansive backyard and pool area.",
+      description: "Private",
       pricePerNight: 2445
      },{
       ownerId: 2,
@@ -32,8 +31,7 @@ module.exports = {
       lat: 37.7645358,
       lng: -122.4730327,
       name: "Spectacular Riverfront",
-      previewImage:'https://a0.muscache.com/im/pictures/prohost-api/Hosting-49194945/original/c49456c8-8608-446c-9f01-3843adc81945.jpeg?im_w=1200',
-      description: "Quite possibly the most spectacular home on The Colorado River! Gated entrance allures you to this estate.",
+      description: "Quite possibly",
       pricePerNight: 2600
      },{
       ownerId: 3,
@@ -44,7 +42,6 @@ module.exports = {
       lat: 37.7645358,
       lng: -122.4730327,
       name: "Black Rock",
-      previewImage:'https://a0.muscache.com/im/pictures/prohost-api/Hosting-48427474/original/6399ce4a-1d68-4d96-a20f-4cedb4c3d254.jpeg?im_w=1200',
       description: "Place where web developers are created",
       pricePerNight: 1483
      },
--- backend/db/seeders/20220922022541-demo-images.js
+++ backend/db/seeders/20220922022541-demo-images.js
@@ -5,15 +5,15 @@ module.exports = {
     await queryInterface.bulkInsert('Images', [{
       reviewImageId: 1,
       spotImageId: 1,
-      url: "image url",
+      url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-47354666/original/b92fc905-70ea-449a-aa95-c79ade3ceadb.jpeg?im_w=1200',
      },{
       reviewImageId: 2,
       spotImageId: 2,
-      url: "image url",
+      url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-49194945/original/c49456c8-8608-446c-9f01-3843adc81945.jpeg?im_w=1200',
      },{
       reviewImageId: 3,
       spotImageId: 3,
-      url: "image url",
+      url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-48427474/original/6399ce4a-1d68-4d96-a20f-4cedb4c3d254.jpeg?im_w=1200',
      },
     ], {});
    
