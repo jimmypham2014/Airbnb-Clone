144d64980f7102d3957ea82e43377ee0d0f5cd06
jimmy <jimmyfam96@gmail.com>
2022-11-05 11:26:09 -0700

    sdfs

--- backend/db/seeders/20220914064950-demo-spot.js
+++ backend/db/seeders/20220914064950-demo-spot.js
@@ -19,6 +19,7 @@ module.exports = {
       country: "United States of America",
       lat: 37.7645358,
       lng: -122.4730327,
+      previewImage: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-48427474/original/6399ce4a-1d68-4d96-a20f-4cedb4c3d254.jpeg?im_w=1200',
       name: "Beverly Hills Maison",
       description: "Private",
       pricePerNight: 2445
@@ -31,6 +32,7 @@ module.exports = {
       lat: 37.7645358,
       lng: -122.4730327,
       name: "Spectacular Riverfront",
+      previewImage: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-47354666/original/b92fc905-70ea-449a-aa95-c79ade3ceadb.jpeg?im_w=1200',
       description: "Quite possibly",
       pricePerNight: 2600
      },{
@@ -42,6 +44,7 @@ module.exports = {
       lat: 37.7645358,
       lng: -122.4730327,
       name: "Black Rock",
+      previewImage: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-49194945/original/c49456c8-8608-446c-9f01-3843adc81945.jpeg?im_w=1200',
       description: "Place where web developers are created",
       pricePerNight: 1483
      },
